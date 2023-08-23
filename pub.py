8bb0a0ee95e8818029691b45573849cdb063884e
Krzysztof Zielinski <kzielins@users.noreply.github.com>
2023-08-23 22:07:25 +0200

    publishe ans sub new version, removed sample

---
+++ pub.py
@@ -0,0 +1,64 @@
+import os,json,time,datetime
+import requests
+import lxml.html
+
+from google.cloud import pubsub_v1
+from google.api_core.exceptions import AlreadyExists
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="exchnage-pubsub-sa.json"
+
+
+PROJECT_ID = "exchnage-pubsub"
+TOPIC_ID = "my-topic"
+DELAY_SLEEP = 5
+
+
+
+def create_topic(project_id, topic_id):
+  """Create pubsub project_id -> topic if not exists"""
+  publisher = pubsub_v1.PublisherClient()
+  #create topic if not exist  
+  topic_path = publisher.topic_path(project_id, topic_id)
+  try:
+    topic = publisher.create_topic(request={"name": topic_path})
+    print(f"Created topic: {topic.name}")
+  except AlreadyExists:
+    #print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    print(f' [WARNING] Topic {topic_id} already exists, ')
+ 
+def publish_data(project_id, topic_id, data_str):
+  """publish data_str to topic_id"""
+  publisher = pubsub_v1.PublisherClient()
+  #create topic if not exist  
+  topic_path = publisher.topic_path(project_id, topic_id)
+  data = data_str.encode("utf-8")
+  # When you publish a message, the client returns a future.
+  future = publisher.publish(topic_path, data)
+  print(f" data: {data_str} publish_data result: {future.result()}") 
+
+
+
+def get_exch(web_http):
+  """get currency exchane rate from url"""
+  html = requests.get(web_http)
+  doc = lxml.html.fromstring(html.content)
+  buy = float(doc.xpath('//*[@data-rates-direction="buy"]/text()')[0].replace(',', '.'))
+  sell = float(doc.xpath('//*[@data-rates-direction="sell"]/text()')[0].replace(',', '.'))
+  forex = float(doc.xpath('//*[@data-rates-direction="forex"]/text()')[0].replace(',', '.'))
+  exch_name =   doc.xpath('//*[@class="bem-chart-intro__current-rate"]/text()')[0].replace(":","").replace('Aktualny kurs', '').replace(" ","").replace("\n","") 
+  parsed_exch={"exchange":exch_name, "buy":buy, "sell":sell, "averange":forex , "datetime":datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+  return parsed_exch
+
+
+#create topic
+create_topic(PROJECT_ID, TOPIC_ID)
+
+while 1:
+    data=get_exch("https://internetowykantor.pl/kurs-euro/")
+    #print(f"exchange values {data}")
+    data_str = json.dumps(data)
+    publish_data(PROJECT_ID, TOPIC_ID, data_str)
+    time.sleep(DELAY_SLEEP)
+
+
+ 
---
+++ sub.py
@@ -0,0 +1,64 @@
+import os,json,time
+
+from concurrent.futures import TimeoutError
+from google.cloud import pubsub_v1
+from google.api_core.exceptions import AlreadyExists
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="exchnage-pubsub-sa.json"
+
+
+# projects/exchnage-pubsub/topics/my-topic
+PROJECT_ID = "exchnage-pubsub"
+TOPIC_ID = "my-topic"
+DELAY_SLEEP = 5
+SUBSCRIPTION_ID="my-sub"
+# Number of seconds the subscriber should listen for messages
+TIMEOUT = 5.0
+DELAY_SLEEP = 5
+
+
+
+def create_subscription(project_id, topic_id, subscription_id):
+  """create subscription if not exist  project_id -> topic if not exists"""
+  subscriber = pubsub_v1.SubscriberClient()
+  subscription_path = subscriber.subscription_path(project_id, subscription_id)
+  try:
+    topic_path = subscriber.topic_path(project_id, topic_id)
+    # The `subscription_path` method creates a fully qualified identifier
+    # in the form `projects/{project_id}/subscriptions/{subscription_id}`
+    with subscriber:
+      subscription = subscriber.create_subscription(
+          request={"name": subscription_path, "topic": topic_path}
+      )
+    print(f"Created subscription: {subscription_path}")
+  except AlreadyExists:
+    #print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    print(f' [WARNING] Subscription subscription_path: {subscription_path} already exists')
+
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    print(f"Received {message.data}.")
+    message.ack()
+ 
+
+create_subscription(PROJECT_ID, TOPIC_ID, SUBSCRIPTION_ID)
+    
+subscriber = pubsub_v1.SubscriberClient()
+# The `subscription_path` method creates a fully qualified identifier
+# in the form `projects/{project_id}/subscriptions/{subscription_id}`
+subscription_path = subscriber.subscription_path(PROJECT_ID, SUBSCRIPTION_ID)
+
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"Listening for messages on {subscription_path}..\n")
+
+# Wrap subscriber in a 'with' block to automatically call close() when done. 
+with subscriber:
+    try:
+        # When `timeout` is not set, result() will block indefinitely,
+        # unless an exception is encountered first.
+        streaming_pull_future.result(timeout=TIMEOUT)
+    except TimeoutError:
+        streaming_pull_future.cancel()  # Trigger the shutdown.
+        streaming_pull_future.result()  # Block until the shutdown is complete.
+ 
